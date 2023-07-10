ff1efbf359f9d1502528d8a505e4bec6b6fd7190
apurbojamananis <apurbojaman.anis@gmail.com>
2023-07-10 22:33:31 +0600

    added sidebar content

--- src/Components/Content/AllTabs.jsx
+++ src/Components/Content/AllTabs.jsx
@@ -12,12 +12,11 @@ const AllTabs = () => {
           <Tab>Overview</Tab>
           <Tab>Notes</Tab>
           <Tab>Announcements</Tab>
-          <Tab>Reviews</Tab>
           <Tab>learnings tools</Tab>
         </TabList>
 
         <TabPanel>
-          <div className="flex gap-5 items-top mt-2 hover:bg-DarkBlue px-5 py-3 w-[50%]">
+          <div className="flex gap-5 items-top mt-10 hover:bg-DarkBlue px-5 py-3 w-[50%] min-h-[400px]">
             <div>
               <FaClock className="text-2xl"></FaClock>
             </div>
@@ -30,7 +29,7 @@ const AllTabs = () => {
                 scheduler.
               </p>
               <div className="mt-5">
-                <button className="bg-gradient-to-r from-sky-500 to-pink-600 py-3 px-3 rounded">
+                <button className="bg-gradient-to-r from-sky-500 to-pink-600 py-3 px-5 rounded">
                   Get Started
                 </button>
                 <button className="ml-5">Dismiss</button>
@@ -39,16 +38,51 @@ const AllTabs = () => {
           </div>
         </TabPanel>
         <TabPanel>
-          <h2>Notes</h2>
+          <div className="min-h-[400px] mt-10">
+            <textarea
+              cols="100"
+              rows="1"
+              className="rounded bg-DarkBlue border py-3 px-5"
+              placeholder="Type your Notes"
+            ></textarea>
+            <br />
+            <button className="bg-gradient-to-r from-sky-500 to-pink-600 py-3 px-5 rounded mt-3 ">
+              Submit Notes
+            </button>
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Announcements</h2>
+          <div className="w-1/3 min-h-[400px] mt-10">
+            <h3 className="text-xl font-semibold">
+              Ultimate Jenkins Guide with Interview Preparation
+            </h3>
+            <p className="mt-5 font-medium">Hello everyone</p>
+            <p className="mt-6 font-semibold">100% Free</p>
+            <p className="mt-5">
+              now our Paid course{" "}
+              <b>${`"Ultimate Jenkins Guide with Interview Preparation "`}</b>{" "}
+              is fully Free and Available on our YouTube Channel
+            </p>
+            <p className="mt-5">
+              check this Playlist we will upload daily three videos and will be
+              available fully for free on our YouTube
+            </p>
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Reviews</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>learnings tools</h2>
+          <div className="w-1/2 min-h-[400px] mt-10">
+            <h2 className="text-2xl font-semibold">Learnings tools</h2>
+            <h3 className="text-xl mt-3">Calendar events</h3>
+            <p className="mt-5">
+              Learning a little each day adds up. Research shows that students
+              who make learning a habit are more likely to reach their goals.
+              Set time aside to learn and get reminders using your learning
+              scheduler.
+            </p>
+            <button className="bg-gradient-to-r from-sky-500 to-pink-600 py-3 px-5 rounded mt-3 ">
+              Schedule Learning time
+            </button>
+          </div>
         </TabPanel>
       </Tabs>
     </div>
--- src/Components/Content/VideoList.jsx
+++ src/Components/Content/VideoList.jsx
@@ -12,7 +12,7 @@ const VideoList = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
           <p>2. Hello World</p>
@@ -21,7 +21,7 @@ const VideoList = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
           <p>3. Data Types</p>
@@ -30,7 +30,7 @@ const VideoList = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
           <p>4. Numbers</p>
@@ -39,55 +39,101 @@ const VideoList = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>5. Numbers</p>
+          <p>5. Math Function</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>6. Numbers</p>
+          <p>6. Operator Precedence</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>7. Numbers</p>
+          <p>7. Variables</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>8. Numbers</p>
+          <p>8. String</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>9. Numbers</p>
+          <p>9. Types Conversion</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
         </div>
       </div>
-      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-3">
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
         <input type="checkbox" className="text-[20px]" />
         <div>
-          <p>10. Numbers</p>
+          <p>10. Escape Sequence</p>
+          <div className="flex items-center gap-2 mt-1">
+            <FaVideo></FaVideo> 2Min{" "}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
+        <input type="checkbox" className="text-[20px]" />
+        <div>
+          <p>11. Formated Strings</p>
+          <div className="flex items-center gap-2 mt-1">
+            <FaVideo></FaVideo> 2Min{" "}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
+        <input type="checkbox" className="text-[20px]" />
+        <div>
+          <p>12. Strings indexes</p>
+          <div className="flex items-center gap-2 mt-1">
+            <FaVideo></FaVideo> 2Min{" "}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
+        <input type="checkbox" className="text-[20px]" />
+        <div>
+          <p>13. Boolen</p>
+          <div className="flex items-center gap-2 mt-1">
+            <FaVideo></FaVideo> 2Min{" "}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
+        <input type="checkbox" className="text-[20px]" />
+        <div>
+          <p>12. Conditional If Else</p>
+          <div className="flex items-center gap-2 mt-1">
+            <FaVideo></FaVideo> 2Min{" "}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-5 items-baseline mt-2 hover:bg-DarkBlue px-5 py-2">
+        <input type="checkbox" className="text-[20px]" />
+        <div>
+          <p>15. For Loop</p>
           <div className="flex items-center gap-2 mt-1">
             <FaVideo></FaVideo> 2Min{" "}
           </div>
